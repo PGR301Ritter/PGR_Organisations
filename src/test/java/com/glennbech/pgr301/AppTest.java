ad65f75cff54f9005d70767e01085be50ce3af17
Ritter <ritand16@student.westerdals.no>
2018-09-17 11:01:40 +0200

    la til ekstra filer

---
+++ src/test/java/com/glennbech/pgr301/AppTest.java
@@ -0,0 +1,50 @@
+package com.glennbech.pgr301;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    public void testString()
+    {
+        String helloworld = "helloworld";
+        assertTrue( helloworld == "helloworld" );
+    }
+
+    public void testInt()
+    {
+        int number = 123;
+        assertTrue( number == 123 );
+    }
+}
